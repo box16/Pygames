973f743134a46b840f65875707d6c8479dd353d3
box16 <inoinobox16@gmail.com>
2022-05-07 16:22:11 +0900

    リファクタ

--- sample_game.py
+++ sample_game.py
@@ -6,54 +6,52 @@ DISPLAY_HEIGHT = 480
 FPS = 30
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-pygame.init()
-
-screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-clock = pygame.time.Clock()
-
-rect = pygame.Rect((0, 0), (8, 8))
-image = pygame.Surface((8, 8))
-image.fill(WHITE)
+MOVEMENT = 3
+MOVEMENT_LIST = [-MOVEMENT, MOVEMENT]
+PARTICLE_SIZE = (8, 8)
 
 
-def get_movement():
-    _random = random.randrange(3)
-    if _random == 0:
-        return -5
-    elif _random == 1:
-        return 0
-    else:
-        return 5
+def get_movement(left, top, width, height):
+    x_movement = random.choice(MOVEMENT_LIST)
+    y_movement = random.choice(MOVEMENT_LIST)
+
+    if ((left+x_movement) < 0) or (left+width+x_movement > DISPLAY_WIDTH):
+        x_movement = 0
+    if ((top+y_movement) < 0) or (top+height+y_movement > DISPLAY_HEIGHT):
+        y_movement = 0
+
+    return x_movement, y_movement
 
 
-def limit_movement(rect, x_movement, y_movement):
-    _left = rect.left
-    _width = rect.width
-    _top = rect.top
-    _height = rect.height
-
-    _x = x_movement
-    _y = y_movement
-
-    if ((_left+x_movement) < 0) or (_left+_width+x_movement > 720):
-            _x = 0
-    if ((_top+y_movement) < 0) or (_top+_height+y_movement > 480):
-        _y = 0
-
-    return _x, _y
+def get_random_pos():
+    x = random.randint(0, DISPLAY_WIDTH)
+    y = random.randint(0, DISPLAY_HEIGHT)
+    return x, y
 
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    clock.tick(FPS)
-    x_movement, y_movement = limit_movement(
-        rect, get_movement(), get_movement())
-    rect.move_ip(x_movement, y_movement)
-    screen.fill(BLACK)
-    screen.blit(image, rect)
-    pygame.display.update()
+if __name__ == "__main__":
+    pygame.init()
+
+    screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    clock = pygame.time.Clock()
+
+    fisrt_x, first_y = get_random_pos()
+    rect = pygame.Rect((fisrt_x, first_y), PARTICLE_SIZE)
+    image = pygame.Surface(PARTICLE_SIZE)
+    image.fill(WHITE)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        clock.tick(FPS)
+
+        x_movement, y_movement = get_movement(
+            rect.left, rect.top, rect.width, rect.height)
+        rect.move_ip(x_movement, y_movement)
+
+        screen.fill(BLACK)
+        screen.blit(image, rect)
+        pygame.display.update()
